514d6b1e746c5e1433a804fcc7594e8f7da523d4
marwaybs <bsmarway@gmail.com>
2019-09-06 14:12:04 -0400

    added authentication and uer model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :items
       resources :lists
+      post 'authenticate', to: 'authentication#authenticate'
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_203645) do
+ActiveRecord::Schema.define(version: 2019_09_06_143652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2019_08_07_203645) do
     t.text "excerpt"
     t.text "description"
     t.integer "upvotes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
